4f903dc5552373ad3c825f1205e66c274db47fd4
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-05-30 12:40:52 +0100

    complete

--- Inventory.js
+++ Inventory.js
@@ -1,3 +1,4 @@
+
 // Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. 
 // Update the current existing inventory item quantities (in arr1). 
 // If an item cannot be found, add the new item and quantity into the inventory array. 
@@ -42,9 +43,6 @@ return arr1.sort(function(a, b) {
     return 0;
   });
 }
-
-
-
 var curInv = [
     [21, "Bowling Ball"],
     [2, "Dirty Sock"],
